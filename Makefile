b6c3ce5bd1b02b7896e94bf564bc34b0751b02b8
Sacha FROMENT <sacha@sacha.house>
2023-11-05 16:02:46 +0100

    doc(env, gitgnore, etc): added useful files

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 server:
 	go run cmd/main.go
+
+# this requires "air" to be installed
+# you can run `go install github.com/cosmtrek/air@latest` to get it
 live:
-	air
+	air 
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+// This file only exists so that tailwind vs-code plugin detects it and
+// provides useful autocomplete for classnames
+// cf : https://tailwindcss.com/docs/configuration
+// >By default, Tailwind will look for an optional tailwind.config.js file
+// >at the root of your project where you can define any customizations.
